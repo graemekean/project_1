0b6bc1a8400495fb3bb6837233752a4efb9c7951
graemekean <graemekean@gmail.com>
2015-11-09 10:10:41 +0000

    sorted commenting with user email to identify

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
   def create
 
-    params[:commenter] = current_user.email
      @photo = Photo.find(params[:photo_id])
-
+     
      @comment = @photo.comments.create(comment_params)
      redirect_to photo_path(@photo)
      
-   end
+  end
   
    private
      def comment_params
+       params[:comment][:commenter] = current_user.email
        params.require(:comment).permit(:commenter, :body)
      end
 

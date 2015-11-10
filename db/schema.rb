f0f8e4cef5e59cdd8183e483443042ca7013610d
graemekean <graemekean@gmail.com>
2015-11-10 15:00:00 +0000

    updated main navbar and added gender to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109134313) do
+ActiveRecord::Schema.define(version: 20151110145835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(version: 20151109134313) do
     t.string   "about_me"
     t.string   "date_of_birth"
     t.integer  "age"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
